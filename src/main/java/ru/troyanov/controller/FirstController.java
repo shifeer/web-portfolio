73c9731290d0c055980841e39de916b87194af24
Dmitriy Troyanov <dtroanov87@gmail.com>
2024-03-02 19:39:12 +0500

    first commit

---
+++ src/main/java/ru/troyanov/controller/FirstController.java
@@ -0,0 +1,16 @@
+package ru.troyanov.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "goodbye";
+    }
+}
